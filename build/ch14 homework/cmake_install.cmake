9c47b344ccf4d1a87298207ebb3265d6f08567d9
Anna <peachtiger0@gmail.com>
2024-06-05 13:12:31 -0400

    ok

--- build/ch14 homework/cmake_install.cmake
+++ build/ch14 homework/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/anna/Documents/ostep/files/ch7 homework
+# Install script for directory: /Users/anna/Documents/ostep/files/ch14 homework
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,6 +34,6 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
---
+++ files/ch14 homework/null.c
@@ -0,0 +1,9 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    int *a = NULL;
+    printf("A is %d\n", *a);
+    return 0;
+}
